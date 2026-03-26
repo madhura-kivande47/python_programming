8a1cf8e2cc2b1ddd0fb5db78f3f65a399f96aee8
madhura-kivande47 <kivandemadhura12@gmail.com>
2026-03-27 05:29:35 +0530

    Add files via upload

---
+++ work2.py
@@ -0,0 +1,28 @@
+#program to understnd how to use the data type
+name='madhura'
+marks=80
+percentage=93.2
+complex_value=2+3j
+passed=True
+print("Name:",name)#madhua
+print("Marks:",marks)#80
+print("Percentage:",percentage)#93.2
+print("complex value:",complex_value)#2+3
+print("Passed status:",passed)#True
+
+#program to find the data type of variable
+name='madhura'
+marks=80
+percentage=93.2
+complex_value=2+3j
+passed=True
+print("Dislay the ouput:")
+print("Name(Data type):",type(name))#<class 'str'>
+print("Marks(Data type):",type(marks))#<class 'int'>
+print("Percentage(Data type):",type(percentage))#<class 'float'>
+print("complex value(Data type):",type(complex_value))#<class 'complex'>
+print("Passed status:",type(passed))#<class 'bool'>
+
+#program to cheak the adress of the variable
+name='Madhura'
+print("Name(Address=):",id(name)) #138858424051504
